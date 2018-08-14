0a8bfd14d61bb4a990b6b53065f6a202331798d9
alexanderpaulino <apaulino127@gmail.com>
2018-08-13 23:43:15 -0400

    changed PORT variable case

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 
-var PORT = process.env.PORT || 8080;
+var port = process.env.PORT || 8080;
 
 var app = express();
 
@@ -23,7 +23,10 @@ app.set("view engine", "handlebars");
 // Import routes and give the server access to them.
 var routes = require("./controllers/burgerController.js");
 
-app.use("/", routes);
+app.use(routes);
 
 // Start our server so that it can begin listening to client requests.
-app.listen(PORT);
+app.listen(port, function() {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + port);
+});
