40330f297d65b0bcd0d440a301d9be59bb082658
alexanderpaulino <apaulino127@gmail.com>
2018-08-13 23:27:45 -0400

    Edited server.js

--- server.js
+++ server.js
@@ -23,10 +23,7 @@ app.set("view engine", "handlebars");
 // Import routes and give the server access to them.
 var routes = require("./controllers/burgerController.js");
 
-app.use(routes);
+app.use("/", routes);
 
 // Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
-  // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
-});
+app.listen(PORT);
